9ca6b79b00eb62512e8e185b49f6f863b07b35fc
MsOluwademilade <oyekanmidemilade2@gmail.com>
2023-03-15 22:21:19 +0100

    Pushing files

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    $fullname = $_POST['fullname'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $sub = $_POST['sub'];
+    $message = $_POST['message'];
+
+    //Database connection
+    
+?>
